78c8a762830bdc6ed552b161138ae3f2b1757ba3
Irina Strelkina <strelaweb@gmail.com>
2022-08-14 10:24:24 +1000

    feat: add images

---
+++ src/header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">Devias Kit</div>
+    </div>
+  );
+};
+export default Header;
